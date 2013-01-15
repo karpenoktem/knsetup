9d998bd161a0814ec5b38fffac40b7db4c83974d
Jille Timmermans <jille@karpenoktem.nl>
2013-01-15 14:52:33 +0100

    Netter omgaan met permissies

--- create-project.sh
+++ create-project.sh
@@ -30,8 +30,9 @@ mkdir $PROJECTS/$NAME/sankhara-union
 mkdir $PROJECTS/$NAME/phassa-union
 unionfs-fuse -o cow,max_files=32768 -o allow_other,use_ino,suid,dev,nonempty $PROJECTS/$NAME/sankhara-union=RW:$TEMPLATES/sankhara-$FSTPL=RO $PROJECTS/$NAME/sankhara
 unionfs-fuse -o cow,max_files=32768 -o allow_other,use_ino,suid,dev,nonempty $PROJECTS/$NAME/phassa-union=RW:$TEMPLATES/phassa-$FSTPL=RO $PROJECTS/$NAME/phassa
-cp -p finish-sankhara-project.sh $PROJECTS/$NAME/sankhara
-cp -p finish-phassa-project.sh $PROJECTS/$NAME/phassa
+cp finish-sankhara-project.sh $PROJECTS/$NAME/sankhara
+cp finish-phassa-project.sh $PROJECTS/$NAME/phassa
+chmod 544 $PROJECTS/$NAME/sankhara/finish-sankhara-project.sh $PROJECTS/$NAME/phassa/finish-sankhara-project.sh
 mount_special_filesystems $PROJECTS/$NAME/sankhara
 mount_special_filesystems $PROJECTS/$NAME/phassa
 chroot $PROJECTS/$NAME/sankhara /finish-sankhara-project.sh $NAME
--- create-template.sh
+++ create-template.sh
@@ -24,11 +24,13 @@ echo sankhara-template > $SANKHARA/etc/debian_chroot
 echo phassa-template > $PHASSA/etc/debian_chroot
 
 cp -p finish-sankhara-template.sh $SANKHARA/finish-sankhara-template.sh
+chmod 544 $SANKHARA/finish-sankhara-template.sh
 mount_special_filesystems $SANKHARA
 chroot $SANKHARA /finish-sankhara-template.sh
 umount_special_filesystems $SANKHARA
 
 cp -p finish-phassa-template.sh $PHASSA/finish-phassa-template.sh
+chmod 544 $PHASSA/finish-phassa-template.sh
 mount_special_filesystems $PHASSA
 chroot $PHASSA /finish-phassa-template.sh
 umount_special_filesystems $PHASSA
