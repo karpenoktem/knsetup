06d094b8f6391015c359289353a99e2665c1d4d2
Jille Timmermans <jille@karpenoktem.nl>
2013-01-15 14:51:11 +0100

    Meer opzetten

---
+++ finish-sankhara-template-infra.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -ev
+
+cd ~infra
+
+git clone git://github.com/karpenoktem/kninfra.git repo
+ln -s repo/bin
+
+mkdir scm
+cd scm
+git clone git://github.com/bwesterb/mirte
+git clone git://github.com/bwesterb/py-tarjan
+git clone git://github.com/karpenoktem/regl
+git clone git://github.com/bwesterb/sarah
+
+cd ..
+mkdir py
+cd py
+git clone git://github.com/awesterb/koert
+for i in mirte py-tarjan regl sarah; do
+	ln -s ../scm/$i
+done
+# XXX iso8601 ophalen
--- finish-sankhara-template.sh
+++ finish-sankhara-template.sh
@@ -13,3 +13,18 @@ addgroup --gid 1001 interinfra
 addgroup --gid 501 fotos
 adduser --uid 1000 --gid 999 infra --gecos ",,," --disabled-password
 adduser --uid 501 --gid 501 fotos --gecos ",,," --disabled-password
+
+sudo -u infra finish-sankhara-template-infra.sh
+
+mkdir -p /srv/karpenoktem.nl/htdocs/
+cd /srv/karpenoktem.nl/htdocs/
+ln -s /mnt/phassa/groups/leden/site-archief archief
+wget -O favicon.ico http://karpenoktem.nl/favicon.ico
+git clone git://github.com/karpenoktem/punbb.git forum
+git clone git://github.com/karpenoktem/knfotos.git fotos
+git clone git://github.com/karpenoktem/mediawiki.git mediawiki
+git clone git://github.com/karpenoktem/knsite site
+
+mkdir -p /var/fotos
+mkdir -p /var/cache/fotos
+chown fotos:fotos /var/fotos /var/cache/fotos
