348203cc3bd834a6c265aca6203a5cd03b521923
Jille Timmermans <jille@karpenoktem.nl>
2013-01-15 23:20:09 +0100

    sankhara:/home/infra/media aanmaken

--- finish-sankhara-fstemplate-infra.sh
+++ finish-sankhara-fstemplate-infra.sh
@@ -7,19 +7,29 @@ cd ~infra
 git clone git://github.com/karpenoktem/kninfra.git repo
 ln -s repo/bin
 
-mkdir scm
-cd scm
-git clone git://github.com/bwesterb/mirte
-git clone git://github.com/bwesterb/py-tarjan
-git clone git://github.com/karpenoktem/regl
-git clone git://github.com/bwesterb/sarah
+(
+	mkdir scm
+	cd scm
+	git clone git://github.com/bwesterb/mirte
+	git clone git://github.com/bwesterb/py-tarjan
+	git clone git://github.com/karpenoktem/regl
+	git clone git://github.com/bwesterb/sarah
+)
 
-cd ..
-mkdir py
-cd py
-git clone git://github.com/awesterb/koert
-for i in mirte py-tarjan regl sarah; do
-	ln -s ../scm/$i
-done
+(
+	mkdir py
+	cd py
+	git clone git://github.com/awesterb/koert
+	for i in mirte py-tarjan regl sarah; do
+		ln -s ../scm/$i
+	done
+	# XXX iso8601 ophalen
+)
 
-# XXX iso8601 ophalen
+(
+	mkdir media
+	cd media
+	for i in base leden reglementen; do
+		ln -s ../repo/kn/$i/media $i
+	done
+)
