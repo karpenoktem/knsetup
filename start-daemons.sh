ff8c9e5abd109f4a35ff459fade330cced953fd6
Jille Timmermans <jille@karpenoktem.nl>
2013-02-10 18:40:37 +0100

    mysql-client package daemons kunnen starten

---
+++ start-daemons.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -ev
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 <name>" >&2
+	exit 1
+fi
+
+case "$1" in
+	--start-sankhara)
+		export PYTHONPATH=/home/infra/py
+		cd /home/infra/repo/utils/
+		screen -d -m -S daan python daan.py
+		su -c "screen -d -m -S infra python giedo.py" infra
+		su -c "screen -X chdir /home/infra/repo/kn/" infra
+		# su -c "screen -X screen python manage.py runserver 0:8004" infra
+		return
+	;;
+	--start-phassa)
+		export PYTHONPATH=/root/py
+		cd /root/kninfra/utils/
+		screen -d -m -S cilia python cilia.py
+		return
+	;;
+esac
+
+NAME="$1"
+PROJECTS=/projects
+
+chroot $PROJECTS/$NAME/phassa /knsetup/start-daemons.sh --start-phassa
+chroot $PROJECTS/$NAME/sankhara /knsetup/start-daemons.sh --start-sankhara
