05dbb6c0f1a87dad2a0089bfcf2518424095a56c
Jille Timmermans <jille@karpenoktem.nl>
2013-03-03 16:39:05 +0100

    php5-cgi installeren

--- finish-sankhara-fstemplate.sh
+++ finish-sankhara-fstemplate.sh
@@ -33,7 +33,7 @@ apt-get install -y equivs
 	dpkg -i *.deb
 )
 
-apt-get install -y git ffmpeg php5-cli php5-mysql php5-memcache php5-curl memcached sudo python python-django python-m2crypto python-mysqldb python-gdata msgpack-python python-pymongo msgpack-python mailman python-pyparsing python-imaging python-markdown python-pip build-essential python-dev mysql-client screen nvi lighttpd python-flup
+apt-get install -y git ffmpeg php5-cli php5-cgi php5-mysql php5-memcache php5-curl memcached sudo python python-django python-m2crypto python-mysqldb python-gdata msgpack-python python-pymongo msgpack-python mailman python-pyparsing python-imaging python-markdown python-pip build-essential python-dev mysql-client screen nvi lighttpd python-flup
 apt-get install -y --no-install-recommends ipython
 # mongodb-10gen gebruiken we voorlopig nog algemeen op khandhas
 # mysql-server moet hier ook nog bij maar die crashen de installatie (en gebruiken we voorlopig nog algemeen op khandhas)
